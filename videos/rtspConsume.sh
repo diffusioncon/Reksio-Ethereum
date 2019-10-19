64dc1ad682c175933904098e973660bf9c595e4c
Kamil Nowak <info@cookiemonsters.pl>
2019-10-19 21:07:03 +0200

    timestamp as filename

--- videos/rtspConsume.sh
+++ videos/rtspConsume.sh
@@ -4,9 +4,12 @@ openRTSP -D 1 -c -B 10000000 -b 10000000 -4 -Q -F CAM -P 10 -t rtsp://$RTSP_IP:$
 
 while true; do
     for file in ./*; do
-        f=${file##*/}
-        if [ $f != "rtspConsume.sh" ]; then
-            path=$(pwd)/$f
+        filename=${file##*/}
+        if [ $filename != "rtspConsume.sh" ]; then
+            dateTime=`date '+%Y_%m_%d__%H_%M_%S'`;
+            filenameWithDateTime="${dateTime}.mp4"
+            path=$(pwd)/$filenameWithDateTime
+            mv $(pwd)/$filename $path
             curl -F "key=$f" -F "file=@$path" -F "Content-Type=multipart/form-data" $SECRETARY_URL
             rm $path
         fi
