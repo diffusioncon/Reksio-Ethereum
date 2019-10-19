393e2db5136310c23151148df1e0fb357ce60147
Kamil Nowak <info@cookiemonsters.pl>
2019-10-19 10:53:02 +0200

    spring boot project

---
+++ src/main/java/dac/reksio/secretary/SecretaryApplication.java
@@ -0,0 +1,13 @@
+package dac.reksio.secretary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dac/reksio/secretary/SecretaryApplicationTests.java
@@ -0,0 +1,13 @@
+package dac.reksio.secretary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
