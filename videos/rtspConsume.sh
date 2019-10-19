1c4b810a5108fa9414a5e2a4e6bfb36c23b63dab
Kamil Nowak <info@cookiemonsters.pl>
2019-10-19 20:54:26 +0200

    rm uploaded video

--- videos/rtspConsume.sh
+++ videos/rtspConsume.sh
@@ -8,6 +8,7 @@ while true; do
         if [ $f != "rtspConsume.sh" ]; then
             path=$(pwd)/$f
             curl -F "key=$f" -F "file=@$path" -F "Content-Type=multipart/form-data" $SECRETARY_URL
+            rm $path
         fi
     done
     sleep 10
