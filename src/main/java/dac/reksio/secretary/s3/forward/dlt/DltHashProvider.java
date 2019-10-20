39b9ccb3f4e37d3bbb5eb9d71064f78cd7f075f5
akotynski <aleksanderkotbury@gmail.com>
2019-10-20 13:39:40 +0200

    fix updating calculating last hash time

--- src/main/java/dac/reksio/secretary/s3/forward/dlt/DltHashProvider.java
+++ src/main/java/dac/reksio/secretary/s3/forward/dlt/DltHashProvider.java
@@ -12,13 +12,8 @@ import java.time.Instant;
 public class DltHashProvider {
 
     private final DltClient dltClient;
-    private final FileRepository fileRepository;
 
     public DltHashDto getHashOfFile(FileEntity file) {
-        DltHashDto dltHashDto = dltClient.getHashOfFile(file.getFilename());
-        FileEntity fileEntity = fileRepository.getOne(file.getId());
-        fileEntity.setHashCalculationDateTime(Instant.now());
-        fileRepository.save(fileEntity);
-        return dltHashDto;
+        return dltClient.getHashOfFile(file.getFilename());
     }
 }
